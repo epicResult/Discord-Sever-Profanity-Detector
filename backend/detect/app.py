1c5bd9efe05abf01f7c26e750db267ec77400d88
HarguntasBenipal <harguntasbenipal16@gmail.com>
2024-06-05 02:05:37 -0400

    migrated to google gemini

--- backend/detect/app.py
+++ backend/detect/app.py
@@ -1,13 +1,14 @@
 import discord
 from openai import OpenAI
 import os
-
+from pathlib import Path
+import hashlib
+import google.generativeai as genai
 
 intents = discord.Intents.default()
 intents.message_content = True
-token = os.environ["TOKEN"]
+token = os.getenv('TOKEN')
 client = discord.Client(intents=intents)
-model = OpenAI()
 
 @client.event
 async def on_ready():
@@ -20,5 +21,23 @@ async def on_message(message):
 
     if message.content.startswith("Hello"):
         await message.channel.send("Hello!")
+
+    genai.configure(api_key=os.getenv('GOOGLE_API'))
+    generation_config = {
+        "temperature": 1,
+         "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 8192,
+    }
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+                              generation_config=generation_config,)
+                              # safety_settings = Adjust safety settings
+                              # See https://ai.google.dev/gemini-api/docs/safety-settings)
+
+    prompt_parts = [
+            "\n This is a discord message from a user: '"+message.content+"'. Does it have profanity?",
+    ]
+    response = model.generate_content(prompt_parts)
+    await message.channel.send(response.text)
 
 client.run(token)
